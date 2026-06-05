485cef505eeba3d99ee0b12d04d5ccd49860d49e
Steven Iannucci <89707282+steveniannucci@users.noreply.github.com>
2026-06-05 22:07:31 +0000

    Jun 5, 2026, 6:07 PM

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,4 @@
 
 const currentYear = new Date().getFullYear()
 
-console.log(`&copy; ${currentYear} All rights reserved. Made by Steven Albert Iannucci`);
+`&copy; ${currentYear} All rights reserved. Made by Steven Albert Iannucci`
