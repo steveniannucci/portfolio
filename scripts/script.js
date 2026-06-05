e64d106702355213d464eb06d7739e6e29706e4e
Steven Iannucci <89707282+steveniannucci@users.noreply.github.com>
2026-06-05 22:42:42 +0000

    Jun 5, 2026, 6:42 PM

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 // scripts/script.js
+
+// function getCopyrightInfo() {
+//     return `&copy; ${new Date().getFullYear()} All rights reserved. Made by Steven Albert Iannucci`
+// }
+
+// function render() {
+//     document.querySelector("#footer").innerHTML += `<p>${getCopyrightInfo()}</p>`
+// }
+
+// render()
