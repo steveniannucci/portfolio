7a158c7f65529da2211697155f10bb9ef0f968c3
Steven Iannucci <89707282+steveniannucci@users.noreply.github.com>
2026-06-05 18:15:55 -0400

    Updated contact form styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,3 @@
 // scripts/script.js
 
-// Looks for the element with the class 'hamburger'.
-const hamburger = document.querySelector('.hamburger');
-// Looks for the <nav> element.
-const nav = document.querySelector('nav');
-
-// Adds a click event listener to the hamburger button.
-hamburger.addEventListener('click', function() {
-    nav.classList.toggle('active');
-    const isExpanded = nav.classList.contains('active');
-    hamburger.setAttribute('aria-expanded', isExpanded);
-    hamburger.classList.toggle('active');
-});
+console.log(`&copy; ${new Date().getFullYear()} All rights reserved. Made by Steven Albert Iannucci`)
